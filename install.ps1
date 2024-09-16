134ca9f34fd10578ad9ae8c9ce5b7374bc46fa45
Thomas Scheibelhofer <ScheibeT@kapsch.net>
2024-09-16 12:17:12 +0200

    Adapt to load from git repository

--- install.ps1
+++ install.ps1
@@ -2,8 +2,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Check if running as Administrator
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "You need to run this script as Administrator!" -ForegroundColor Red
     exit
 }
@@ -12,23 +11,42 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 Write-Host "Installing Chocolatey..." -ForegroundColor Green
 Set-ExecutionPolicy Bypass -Scope Process -Force; `
     [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; `
-    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Refresh environment variables
-Write-Host "Refreshing environment variables..." -ForegroundColor Green
-RefreshEnv
 
-# Install applications
-$packages = @(
-    "7zip",
-    "googlechrome",
-    "filezilla",
-    "putty"
-)
+    
 
-foreach ($package in $packages) {
-    Write-Host "Installing $package..." -ForegroundColor Green
-    choco install $package -y
+# # Refresh environment variables
+# Write-Host "Refreshing environment variables..." -ForegroundColor Green
+# Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+# refreshenv
+
+# Define the URL of the CSV file in the GitHub repository
+$csvUrl = "https://raw.githubusercontent.com/Henchway/Choco-avd/main/apps.csv"
+
+# Define the path to save the CSV file locally
+$localCsvPath = "$PSScriptRoot\apps.csv"
+
+# Download the CSV file
+Write-Host "Downloading CSV from $csvUrl..."
+Invoke-WebRequest -Uri $csvUrl -OutFile $localCsvPath
+
+# Load the CSV file
+$apps = Import-Csv -Path $localCsvPath
+
+# Loop through each app and print the app and version
+foreach ($app in $apps) {
+    Write-Host "App: $($app.name), Version: $($app.version)"
+}
+
+foreach ($app in $apps) {
+    Write-Host "Installing $app..." -ForegroundColor Green
+    if ($app.version -eq "") {
+        choco install $app.name -y --version $app.version
+    }
+    else {
+        choco install $app.name -y
+    }
 }
 
 Write-Host "All applications have been installed successfully." -ForegroundColor Green
